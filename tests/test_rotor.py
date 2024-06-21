7fb622036610d6812f4baa0b9cec29ca30b2c04e
ashal-ali <syedashalali95@gmail.com>
2024-06-21 18:01:09 +0500

    Edited tests

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -1,5 +1,8 @@
 import unittest
-import Rotor
+import sys 
+import os 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from enigma.rotor import Rotor
 
 
 class TestRotor(unittest.TestCase):
